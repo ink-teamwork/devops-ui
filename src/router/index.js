e06d4b11fb2f93d5719a1013c67c369ab7e43fbb
zhaokai <985144332@qq.com>
2019-03-10 19:54:29 +0800

    添加导航守卫做鉴权判断

--- src/router/index.js
+++ src/router/index.js
@@ -6,4 +6,19 @@ const router = new VueRouter({
     routes // short for `routes: routes`
 });
 
+router.beforeEach((to, from, next) => {
+    const auth = JSON.parse(sessionStorage.getItem("auth"));
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!auth.loggedIn) {
+            next({
+                path: '/login',
+                query: { redirect: to.fullPath }
+            })
+        } else {
+            next()
+        }
+    }
+    next()
+})
+
 export default router;
